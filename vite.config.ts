0e8afce5ee4886ef30a8db98bc0993c5e48e3712
Kirk Scheibelhut <kjs@scheibo.com>
2022-11-22 10:32:48 -0800

    clean

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,15 @@ import {defineConfig} from 'vite';
 
 const NODE_MODULES = '/node_modules/';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const parent = dirname(__dirname);
+const root = dirname(fileURLToPath(import.meta.url));
+const parent = dirname(root);
 
 export default defineConfig({
   build: {
-    cssCodeSplit: false,
-    sourcemap: false,
     chunkSizeWarningLimit: Infinity,
+    cssCodeSplit: false,
+    minify: true,
+    sourcemap: false,
     rollupOptions: {
       onwarn(warning, warn) {
         if (warning.code !== 'EVAL') warn(warning);
@@ -26,16 +27,13 @@ export default defineConfig({
               ? `${id.slice(1, index)}.${id.slice(index + 1, id.indexOf('/', index + 1))}`
               : id.slice(0, index));
           }
-          if (!(id.startsWith(__dirname) || id.startsWith('vite'))) {
+          if (!(id.startsWith(root) || id.startsWith('vite'))) {
             if (!id.startsWith(parent)) throw new Error(`Unexpected module ${id}`);
             id = id.slice(parent.length + 1);
             index = id.indexOf('/');
-
             const dir = id.slice(0, index);
-            switch (dir) {
-            case '0-ERROR': case 'engine': case 'dmg': return `pkmn.${dir}`;
-            default: return `pkmn.${id.slice(index + 1, id.indexOf('/', index + 1))}`;
-            }
+            const subdir = id.slice(index + 1, id.indexOf('/', index + 1));
+            return ['build', 'dist'].includes(subdir) ? dir : `${dir}.${subdir}`;
           }
         },
       },
