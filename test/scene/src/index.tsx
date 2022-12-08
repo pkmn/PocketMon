42b00ee4e5085b45e7e9e199eaee0d4afde25bd7
Kirk Scheibelhut <kjs@scheibo.com>
2022-12-07 17:21:06 -0800

    add gen 3 mocks

--- test/scene/src/index.tsx
+++ test/scene/src/index.tsx
@@ -1,17 +1,27 @@
 const gen = 1;
 
-const srcs = ['red', 'silver'];
+const srcs = ['emerald', 'silver', 'emerald'];
 const url = 'https://play.pokemonshowdown.com/sprites/';
-const p1 = [['Bulbasaur', 'gen1-back/bulbasaur.png'], ['Cynaquil', 'gen2-back/cyndaquil.png']];
-const p2 = [['Charmander', 'gen1rb/charmander.png'], ['Sentret', 'gen2s/sentret.png']];
+const p1 = [
+  ['Bulbasaur', 'gen1-back/bulbasaur.png'],
+  ['Cynaquil', 'gen2-back/cyndaquil.png'],
+  ['Salamence', 'gen3-back/salamence.png'],
+];
+const p2 = [
+  ['Charmander', 'gen1rb/charmander.png'],
+  ['Sentret', 'gen2s/sentret.png'],
+  ['Scizor', 'gen3/scizor.png'],
+];
 
 const root = document.getElementById('root')!;
 root.appendChild(
-  <div className={`gen${gen} scene`}>
-    <img className="mock" src={`/scenes/${srcs[gen - 1]}.png`} />
-    <span className="p1">{p1[gen - 1][0]}</span>
-    <img className="p1 pixelated" src={`${url}${p1[gen - 1][1]}`} />
-    <img className="p2 pixelated" src={`${url}${p2[gen - 1][1]}`} />
-    <span className="p2">{p2[gen - 1][0]}</span>
+  <div className={`gen${gen}`}>
+    <div className="scene">
+      <img className="mock" src={`/mocks/${srcs[gen - 1]}.png`} />
+      <span className="p1">{p1[gen - 1][0]}</span>
+      <img className="p1 pixelated" src={`${url}${p1[gen - 1][1]}`} />
+      <img className="p2 pixelated" src={`${url}${p2[gen - 1][1]}`} />
+      <span className="p2">{p2[gen - 1][0]}</span>
+    </div>
   </div>
 );
