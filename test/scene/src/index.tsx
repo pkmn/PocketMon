5e4ff15bc7c8dbd594cfe815fbf31b2cc1c24aca
Kirk Scheibelhut <kjs@scheibo.com>
2022-12-07 16:51:56 -0800

    begin experimenting with battle scenes

--- test/scene/src/index.tsx
+++ test/scene/src/index.tsx
@@ -1,2 +1,17 @@
+const gen = 1;
+
+const srcs = ['red', 'silver'];
+const url = 'https://play.pokemonshowdown.com/sprites/';
+const p1 = [['Bulbasaur', 'gen1-back/bulbasaur.png'], ['Cynaquil', 'gen2-back/cyndaquil.png']];
+const p2 = [['Charmander', 'gen1rb/charmander.png'], ['Sentret', 'gen2s/sentret.png']];
+
 const root = document.getElementById('root')!;
-root.appendChild(<div>Hello, world!</div>);
+root.appendChild(
+  <div className={`gen${gen} scene`}>
+    <img className="mock" src={`/scenes/${srcs[gen - 1]}.png`} />
+    <span className="p1">{p1[gen - 1][0]}</span>
+    <img className="p1 pixelated" src={`${url}${p1[gen - 1][1]}`} />
+    <img className="p2 pixelated" src={`${url}${p2[gen - 1][1]}`} />
+    <span className="p2">{p2[gen - 1][0]}</span>
+  </div>
+);
